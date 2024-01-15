7f2e9e42f0e7d5c72f6230302d93b082b03ccd56
syedeasin <syedeasin1000@gmail.com>
2024-01-15 19:24:26 +0600

    Into Screen, Sign In Complete, Working on Home Screen

---
+++ app/src/main/java/com/example/movieapp/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.movieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText userEdit, passEdit;
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initView();
+    }
+
+    private void initView() {
+        userEdit = findViewById(R.id.editTextText);
+        passEdit = findViewById(R.id.editTextPassword);
+        loginBtn = findViewById(R.id.loginbtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(userEdit.getText().toString().isEmpty()){
+                    Toast.makeText(LoginActivity.this,"Please Fill Your User and Password", Toast.LENGTH_SHORT).show();
+                } else if (userEdit.getText().toString().equals("test") && passEdit.getText().toString().equals("test")) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "Your User and Password is not correct", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
