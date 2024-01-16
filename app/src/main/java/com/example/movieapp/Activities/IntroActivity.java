34339cd77bf6a41758ee1aa8c5c6329d716f24b6
syedeasin <syedeasin1000@gmail.com>
2024-01-16 20:08:25 +0600

    Bottom Bar

--- app/src/main/java/com/example/movieapp/Activities/IntroActivity.java
+++ app/src/main/java/com/example/movieapp/Activities/IntroActivity.java
@@ -1,4 +1,4 @@
-package com.example.movieapp;
+package com.example.movieapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.movieapp.R;
 
 public class IntroActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/movieapp/Domain/SliderItems.java
@@ -0,0 +1,12 @@
+package com.example.movieapp.Domain;
+
+public class SliderItems {
+    private int image;
+    public int getImage(int image) {
+        return image;
+    }
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+}
